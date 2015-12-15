c1fd5dd724218165f2cb9bd7a10a511ed8b915e0
Ilya Beda <ir4y.ix@gmail.com>
2015-12-15 22:58:12 +0700

    Fix typo

--- Main.elm
+++ Main.elm
@@ -14,6 +14,6 @@ valueL : Focus.Focus Store String
 valueL = Focus.create .value (\f r -> { r | value = f r.value })
 
 view : Cursor Store Store -> Html.Html
-view cursor = p [] [text "Hello"]
+view cursor = p [] [text (getC (cursor >=> valueL))]
 
 main = drawC {value = "foo"} view
