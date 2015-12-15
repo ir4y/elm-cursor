b77d8a9c793fb123cb9dbe53c8a8276020825fff
Ilya Beda <ir4y.ix@gmail.com>
2015-12-15 23:06:16 +0700

    Two way data-binding PoC

--- Main.elm
+++ Main.elm
@@ -3,7 +3,9 @@ module Main where
 import Cursor exposing(..)
 import Focus exposing((=>))
 
-import Html exposing (Html, text, p)
+import Html exposing (Html, text, p, div)
+import Html.Attributes
+import Html.Events exposing (on, targetValue)
 
 
 type alias Store =
@@ -13,7 +15,16 @@ type alias Store =
 valueL : Focus.Focus Store String
 valueL = Focus.create .value (\f r -> { r | value = f r.value })
 
+input : Cursor a String-> Html
+input cursor = Html.input [ Html.Attributes.value (getC cursor)
+                          , on "input" targetValue (setC cursor)
+                          ] []
+
 view : Cursor Store Store -> Html.Html
-view cursor = p [] [text (getC (cursor >=> valueL))]
+view cursor = let
+                 value = (cursor >=> valueL)
+              in div[] [ p [] [text (getC value)]
+                       , input value
+                       ]
 
 main = drawC {value = "foo"} view
