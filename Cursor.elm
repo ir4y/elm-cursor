b11e2fd9f59fa95a9f9105b613798af2d05773da
Ilya Beda <ir4y.ix@gmail.com>
2015-12-17 08:47:49 +0700

    Style fixes

--- Cursor.elm
+++ Cursor.elm
@@ -4,36 +4,51 @@ import Array
 import Focus exposing ((=>))
 import Maybe
 
+
 type alias Cursor a b =
   { receiver : Signal.Address a
   , state : a
   , lens : Focus.Focus a b
   }
 
+
 (>=>) : Cursor a b -> Focus.Focus b c -> Cursor a c
-(>=>) c1 f1 = { c1 | lens = (c1.lens => f1) }
+(>=>) c1 f1 =
+  { c1 | lens = (c1.lens => f1) }
+
 
 idL : Focus.Focus a a
-idL = Focus.create identity (\f r -> f r)
+idL =
+  Focus.create identity (\f r -> f r)
+
 
 createC : Signal.Mailbox a -> a -> Cursor a a
-createC mailbox state = { receiver = mailbox.address
-                        , state = state
-                        , lens = idL
-                        }
+createC mailbox state =
+  { receiver = mailbox.address
+  , state = state
+  , lens = idL
+  }
+
 
 setC : Cursor a b -> b -> Signal.Message
-setC c1 v =  Signal.message c1.receiver (Focus.set c1.lens v c1.state)
+setC c1 v =
+  Signal.message c1.receiver (Focus.set c1.lens v c1.state)
+
 
 updateC : Cursor a b -> (b -> b) -> Signal.Message
-updateC c1 f = Signal.message c1.receiver (Focus.update c1.lens f c1.state)
+updateC c1 f =
+  Signal.message c1.receiver (Focus.update c1.lens f c1.state)
+
 
 getC : Cursor a b -> b
-getC c1 = Focus.get c1.lens c1.state
+getC c1 =
+  Focus.get c1.lens c1.state
+
 
 drawC : a -> (Cursor a a -> b) -> Signal.Signal b
-drawC state view = let
-                       mailbox = Signal.mailbox state
-                       cursor = createC mailbox
-                   in
-                       Signal.map (cursor >> view) mailbox.signal
+drawC state view =
+  let
+     mailbox = Signal.mailbox state
+     cursor = createC mailbox
+  in
+     Signal.map (cursor >> view) mailbox.signal
--- Main.elm
+++ Main.elm
@@ -2,50 +2,61 @@ module Main where
 
 import Cursor exposing(..)
 import Focus exposing((=>))
-
 import Html exposing (Html, text, p, div, button)
 import Html.Attributes as Attributes
 import Html.Events exposing (on, targetValue)
+import Json.decode as Json
 
-import Json.Decode as Json
 
 type alias Store =
   { value: String
   , counter: Int
   }
 
+
 valueL : Focus.Focus Store String
-valueL = Focus.create .value (\f r -> { r | value = f r.value })
+valueL =
+  Focus.create .value (\f r -> { r | value = f r.value })
 
 counterL : Focus.Focus Store Int
-counterL = Focus.create .counter (\f r -> { r | counter = f r.counter })
+counterL =
+  Focus.create .counter (\f r -> { r | counter = f r.counter })
 
 view : Cursor Store Store -> Html.Html
-view cursor = let
-                 value = cursor >=> valueL
-                 counter = cursor >=> counterL
-              in div [] [ view_input value
-                        , view_counter counter
-                        ]
+view cursor =
+  let
+     value = cursor >=> valueL
+     counter = cursor >=> counterL
+  in div [] [ view_input value
+            , view_counter counter
+            ]
 
 input : Cursor a String -> Html
-input cursor = Html.input [ Attributes.value <| getC cursor
-                          , on "input" targetValue <| setC cursor
-                          ] []
+input cursor =
+    Html.input [ Attributes.value <| getC cursor
+               , on "input" targetValue <| setC cursor
+               ] []
 
 view_input : Cursor Store String -> Html.Html
-view_input cursor = div [] [ p [] [text <| getC cursor]
-                           , input cursor
-                           ]
+view_input cursor =
+  div [] [ p [] [text <| getC cursor]
+                , input cursor
+                ]
 
-dec a = a - 1
-inc a = a + 1
+_dec a =
+  a - 1
+_inc a =
+  a + 1
+
 
 view_counter : Cursor Store Int -> Html.Html
-view_counter cursor = div []
-                          [ button [ on "click" Json.value <| \_ -> updateC cursor dec] [ text "-" ]
-                          , div [] [ text << toString <| getC cursor]
-                          , button [ on "click" Json.value <| \_ -> updateC cursor inc] [ text "+" ]
-                          ]
+view_counter cursor =
+  div []
+      [ button [ on "click" Json.value <| \_ -> updateC cursor _dec] [ text "-" ]
+      , div [] [ text << toString <| getC cursor]
+      , button [ on "click" Json.value <| \_ -> updateC cursor _inc] [ text "+" ]
+      ]
 
-main = drawC {value = "foo", counter = 0} view
+
+main =
+  drawC {value = "foo", counter = 0} view
