6d777a9e916f606973959b21d6b311995e6c76a7
Ilya Beda <ir4y.ix@gmail.com>
2015-12-21 23:24:35 +0700

    Add style

--- examples/src/Counter/Counter.elm
+++ examples/src/Counter/Counter.elm
@@ -2,6 +2,7 @@ module Counter.Counter (view) where
 
 import Cursor exposing(..)
 import Html exposing (Html, text, div, button)
+import Html.Attributes exposing (style)
 import Html.Events exposing (on)
 import Json.Decode as Json
 
@@ -19,6 +20,17 @@ view: Cursor a Int -> Html.Html
 view cursor =
   div []
         [ button [ on "click" Json.value <| \_ -> updateC cursor dec_] [ text "-" ]
-        , div [] [ text << toString <| getC cursor]
+        , div [ countStyle ] [ text << toString <| getC cursor]
         , button [ on "click" Json.value <| \_ -> updateC cursor inc_] [ text "+" ]
         ]
+
+
+countStyle : Html.Attribute
+countStyle =
+  style
+    [ ("font-size", "20px")
+    , ("font-family", "monospace")
+    , ("display", "inline-block")
+    , ("width", "50px")
+    , ("text-align", "center")
+    ]
